5b7e9c90387e37f7afb685bad9967680b6ca966e
Joaquin Rossi <joaquin@joaquinrossi.net>
2021-01-20 14:22:01 -0300

    tabs to spaces and timeout added

--- xkcdl.sh
+++ xkcdl.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 url='https://xkcd.com'
+timeout=1
 
 usage()
 {
 cat << EOF
 usage: xkcdl [-h] [-l] [-d NUM] [-a]
 arguments:
-	-h			show this help message and exit
+    -h          show this help message and exit
 	-n NUM		download specified comic by number
 	-l			download latest comic
 	-a			download all comics
+	-t TIME     set timeout (low timeouts may get you banned)
 EOF
 }
 
@@ -26,15 +28,19 @@ latest()
 	echo $((prev+1))
 }
 
-while getopts 'hn:la' flag
+while getopts 'ht:n:la' flag
 do
 	case "$flag"
 	in
-		h) usage ; exit ;;
-		n) download "${OPTARG}" ; exit ;;
-		l) download "$(latest)" ; exit ;;
-		a) for i in $(seq "$(latest)"); do download "$i"; done ; exit ;;
-		*) exit ;;
+		h) usage; exit;;
+
+		t) timeout="${OPTARG}";;
+
+		n) download "${OPTARG}"; exit;;
+		l) download "$(latest)"; exit;;
+		a) for i in $(seq "$(latest)"); do download "$i" && sleep "$timeout"; done; exit;;
+
+		*) exit;;
 	esac
 done
 
