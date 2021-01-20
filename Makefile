197cbc7bb630c234e8d5bab7bc1477ca95cf5fca
Joaquin Rossi <joaquin@joaquinrossi.net>
2021-01-20 15:02:06 -0300

    actually switch to spaces

--- Makefile
+++ Makefile
@@ -1,21 +1,21 @@
 PREFIX=/usr/local
 
 xkcdl: xkcdl.sh
-	cat xkcdl.sh > $@
-	chmod +x $@
+    cat xkcdl.sh > $@
+    chmod +x $@
 
 test: xkcdl.sh
-	shellcheck -s sh xkcdl.sh
+    shellcheck -s sh xkcdl.sh
 
 clean:
-	rm -f xkcdl
+    rm -f xkcdl
 
 install: xkcdl
-	mkdir -p $(DESTDIR)$(PREFIX)/bin
-	cp -f xkcdl $(DESTDIR)$(PREFIX)/bin
-	chmod 755 $(DESTDIR)$(PREFIX)/bin/xkcdl
+    mkdir -p $(DESTDIR)$(PREFIX)/bin
+    cp -f xkcdl $(DESTDIR)$(PREFIX)/bin
+    chmod 755 $(DESTDIR)$(PREFIX)/bin/xkcdl
 
 uninstall:
-	rm -f $(DESTDIR)$(PREFIX)/bin/xkcdl
+    rm -f $(DESTDIR)$(PREFIX)/bin/xkcdl
 
 .PHONY: test clean install uninstall
--- xkcdl.sh
+++ xkcdl.sh
@@ -8,40 +8,40 @@ cat << EOF
 usage: xkcdl [-h] [-l] [-d NUM] [-a]
 arguments:
     -h          show this help message and exit
-	-n NUM		download specified comic by number
-	-l			download latest comic
-	-a			download all comics
-	-t TIME     set timeout (low timeouts may get you banned)
+    -n NUM      download specified comic by number
+    -l          download latest comic
+    -a          download all comics
+    -t TIME     set timeout (low timeouts may get you banned)
 EOF
 }
 
 download()
 {
-	image=$(curl -Ls "$url/$1/" | grep "URL" | awk '{print $5}')
-	curl -Lso "$1"."${image##*.}" "$image"
-	echo "xkcd #$1 from $image downloaded"
+    image=$(curl -Ls "$url/$1/" | grep "URL" | awk '{print $5}')
+    curl -Lso "$1"."${image##*.}" "$image"
+    echo "xkcd #$1 from $image downloaded"
 }
 
 latest()
 {
-	prev=$(curl -Ls "$url" | grep "prev" | head -n 1 | awk '{print $3}' | grep -Eo '[0-9]+')
-	echo $((prev+1))
+    prev=$(curl -Ls "$url" | grep "prev" | head -n 1 | awk '{print $3}' | grep -Eo '[0-9]+')
+    echo $((prev+1))
 }
 
 while getopts 'ht:n:la' flag
 do
-	case "$flag"
-	in
-		h) usage; exit;;
+    case "$flag"
+    in
+        h) usage; exit;;
 
-		t) timeout="${OPTARG}";;
+        t) timeout="${OPTARG}";;
 
-		n) download "${OPTARG}"; exit;;
-		l) download "$(latest)"; exit;;
-		a) for i in $(seq "$(latest)"); do download "$i" && sleep "$timeout"; done; exit;;
+        n) download "${OPTARG}"; exit;;
+        l) download "$(latest)"; exit;;
+        a) for i in $(seq "$(latest)"); do download "$i" && sleep "$timeout"; done; exit;;
 
-		*) exit;;
-	esac
+        *) exit;;
+    esac
 done
 
 usage
