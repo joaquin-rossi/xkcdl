711cac02067efc196726ef339e5cc7c2f4d84d75
Joaquín Rossi <joaquin@joaquinrossi.net>
2021-02-20 10:02:18 -0300

    [feat]: use xkcd's json interface instead of parsing the website

--- xkcdl.sh
+++ xkcdl.sh
@@ -7,25 +7,24 @@ usage()
 	cat <<- EOF
 	usage: xkcdl [-h] [-l] [-d NUM] [-a] [-t TIME]
 	arguments:
-	    -h          show this help message and exit
-	    -n NUM      download specified comic by number
-	    -l          download latest comic
-	    -a          download all comics
-	    -t TIME     set timeout (low timeouts may get you banned)
+	    -h        show this help message and exit
+	    -n NUM    download specified comic by number
+	    -l        download latest comic
+	    -a        download all comics
+	    -t TIME   set timeout (low timeouts may get you banned)
 	EOF
 }
 
 download()
 {
-    image=$(curl -Ls "$url/$1/" | grep "URL" | awk '{print $5}')
-    curl -Lso "$1"."${image##*.}" "$image"
+    image=$(curl -Ls "$url/$1/info.0.json" | jq .img)
+    curl -Lso "$1.${image##*.}" "$image"
     echo "xkcd #$1 from $image downloaded"
 }
 
 latest()
 {
-    prev=$(curl -Ls "$url" | grep "prev" | head -n 1 | awk '{print $3}' | grep -Eo '[0-9]+')
-    echo $((prev+1))
+    echo $(curl -s "$url/info.0.json" | jq .num)
 }
 
 while getopts 'ht:n:la' flag
@@ -39,8 +38,6 @@ do
         n) download "${OPTARG}"; exit;;
         l) download "$(latest)"; exit;;
         a) for i in $(seq "$(latest)"); do download "$i" && sleep "$timeout"; done; exit;;
-
-        *) usage;;
     esac
 done
 
