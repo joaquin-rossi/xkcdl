2f52e6d86bb1335674de1ba8b0d454d555e2560a
Joaquín Rossi <joaquin@joaquinrossi.net>
2021-02-20 10:02:18 -0300

    feat: use xkcd's json interface instead of parsing the website

--- xkcdl.sh
+++ xkcdl.sh
@@ -7,24 +7,24 @@ usage()
 	cat <<- EOF
 	usage: xkcdl [-t TIME] [-h] [-l] [-d NUM] [-a] 
 	arguments:
-	    -t TIME   set timeout (low values may get you banned) [must be 1st argument]
 	    -h        show this help message and exit
 	    -n NUM    download specified comic by number
 	    -l        download latest comic
 	    -a        download all comics
+	    -t TIME   set timeout (low timeouts may get you banned)
 	EOF
 }
 
 download()
 {
-    image=$(curl -Ls "$url/$1/info.0.json" | jq .img | sed 's/"//g')
+    image=$(curl -Ls "$url/$1/info.0.json" | jq .img)
     curl -Lso "$1.${image##*.}" "$image"
     echo "xkcd #$1 from $image downloaded"
 }
 
 latest()
 {
-    curl -s "$url/info.0.json" | jq .num
+    echo $(curl -s "$url/info.0.json" | jq .num)
 }
 
 while getopts 'ht:n:la' flag
@@ -38,7 +38,5 @@ do
         n) download "${OPTARG}"; exit;;
         l) download "$(latest)"; exit;;
         a) for i in $(seq "$(latest)"); do download "$i" && sleep "$timeout"; done; exit;;
-	
-	*) usage; exit;;
     esac
 done
